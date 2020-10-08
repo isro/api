3fb0fe0d5fed3ec7a616de1d95ccdf0e8458a2b3
Pramod Devireddy <devireddy.pramod@gmail.com>
2020-10-08 08:10:28 +0530

    Added a few customer satellites details (#6)

---
+++ api/customer_satellites.js
@@ -0,0 +1,32 @@
+/*
+ * File         : customer_satellites.js
+ * Project      : NetMon
+ * Created Date : Thursday, Oct 8th 2020, 8:04:33 AM
+ * Author       : Pramod Devireddy
+ * 
+ * Last Modified: Thursday, 8th October 2020 8:05:01 am
+ * Modified By  : Pramod Devireddy
+ * 
+ * Copyright (c)2020 - Pramod Devireddy
+ * ************************* Description *************************
+ *  
+ * ***************************************************************
+ */
+
+
+const fs = require("fs");
+
+var launchers = require("../data/customer_satellites.json");
+
+module.exports = async (req, res) => {
+    try {
+        res.send(launchers);
+    } catch (error) {
+        res.status(500);
+        const response = error.response || {};
+        res.send({
+            message: error.message,
+            response,
+        });
+    }
+};
