e34aa6465c0b3dbdb0c261096ff908e8099cf0a6
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2022-12-21 17:05:46 +0530

    added comments for the customer_satellites.js file

--- api/customer_satellites.js
+++ api/customer_satellites.js
@@ -1,16 +1,20 @@
 const fs = require("fs");
 
+// Load the customer satellite data from a JSON file
 let launchers = require("../data/customer_satellites.json");
 
+// Export an async function to handle requests
 module.exports = async (req, res) => {
-    try {
-        res.send(launchers);
-    } catch (error) {
-        res.status(500);
-        const response = error.response || {};
-        res.send({
-            message: error.message,
-            response,
-        });
-    }
+  try {
+    // Send the customer satellite data as the response
+    res.send(launchers);
+  } catch (error) {
+    // If there is an error, send a 500 status code and the error message and response
+    res.status(500);
+    const response = error.response || {};
+    res.send({
+      message: error.message,
+      response,
+    });
+  }
 };
