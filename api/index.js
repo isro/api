9ce3fb368bef9772b38a7b98e9a2a3e23e72b2b8
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2022-12-21 17:07:33 +0530

    added comments for the index.js file

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
+// Export an async function to handle requests
 module.exports = async (req, res) => {
-    try {
-        res.send("<pre> ISRO API v0.1.0 </pre>");
-    } catch (error) {
-        res.status(500);
-        const response = error.response || {};
-        res.send({
-            message: error.message,
-            response,
-        });
-    }
+  try {
+    // Send a message as the response
+    res.send("<pre> ISRO API v0.1.0 </pre>");
+  } catch (error) {
+    // If there is an error, send a 500 status code and the error message and response
+    res.status(500);
+    const response = error.response || {};
+    res.send({
+      message: error.message,
+      response,
+    });
+  }
 };
